1525ca742f210959626ef63e33c24a39af86ff91
J-E Castagnede <j.e.castagnede@gmail.com>
2020-10-28 15:10:54 +0100

    fix function working in django 3.2

--- geostore_routing/functions.py
+++ geostore_routing/functions.py
@@ -1,5 +1,6 @@
+from django.contrib.gis.db.models import GeometryField
 from django.contrib.gis.db.models.functions import GeoFunc
 
 
 class LineSubstring(GeoFunc):
-    pass
+    output_field = GeometryField()
